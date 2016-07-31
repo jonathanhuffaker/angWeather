13328adacd10f38573c7783dfaa88ebe998281d6
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-07-31 14:25:30 -0500

    firebase authorization almost set up.  Next step is adding login controller

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
 var app = angular.module('angWeather', []);
+
+app.controller("TestCtrl", function($scope){
+	$scope.title = "Testing setup";
+	$scope.tests =[
+	{name:  "test number one dude"},
+	{name:  "number two"},
+	{name:  "tres"},
+	{name:  "Four the love of humanity..."}
+
+	]
+
+});
---
+++ scripts/auth.js
@@ -0,0 +1,63 @@
+var auth = angular.module('Authorize', ['firebase']);
+
+	var name;
+	var profilepic;
+	auth.factory("Auth", ["$firebaseAuth",
+	  function($firebaseAuth) {
+	    var ref = new Firebase("https://angweather-8c736.firebaseio.com");
+
+	    return $firebaseAuth(ref);
+
+	  }
+	]);
+
+	//create a user
+	app.controller('authuserCtrl',
+		['$scope',
+		'Auth',
+		'$firebaseArray',
+		'$location',
+		function($scope, Auth, $currentAcct, $location){
+			$scope.createUser = function(){
+				Auth.$createUser({
+					email: $scope.email,
+					password: $scope.password
+				}).then(function(userData){
+					
+					// Changing what i had originaly to reflect order by child - keeping old for safety
+					var ref = new Firebase("https://angweather-8c736.firebaseio.com/users/"+userData.uid);
+
+
+					ref.set({
+						email: $scope.email,
+						name: $scope.name,
+						uid: userData.uid,
+						
+
+					});
+					console.log("user created with id:" +userData.uid);
+  					$scope.loginUser();
+				}).catch(function(error){
+					console.log("user not created with error:" +error);
+				});
+			};
+			$scope.loginUser = function(){
+				Auth.$authWithPassword({
+					email: $scope.email,
+					password: $scope.password
+				}).then(function(userData){
+					var ref = new Firebase("https://angweather-8c736.firebaseio.com/");
+					console.log("user logged in with id:" +userData.uid);
+					$location.path("/profile/");
+				}).catch(function(error){
+					console.log("user not logged with error:" +error);
+				});
+			};
+
+			$scope.logOut = function(){
+				Auth.$unauth();
+				console.log("user logged out");
+				//redirect back to login page
+				$location.path("/");
+			};
+		}]);
